5fd57736f0cd3bf81ca499b0106d346a75d2cdb6
Leander Pyrz <konsti@niklascfw.de>
2024-10-05 14:04:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,23 @@ import random
 app = FastAPI(
     title="api.lndr.tech"
 )
+
 @app.get("/", description="Root endpoint")
 def get_root():
     return {"message": "Welcome to my API! For more information, visit /docs."}
 
-@app.get("/v1/zitat")
-def zitat():
-    zitate = [
-    "Auf drei sagen wir alle 'Hamburger Royale TS'"
-]
-    random_zitat = random.choice(zitate)
-    return {"zitat": random_zitat}
+@app.get("/v1/joke")
+def zitat(language: str = None):
+    jokes = {
+        "de": ["Wissenschaftler haben herausgefunden... - und sind wieder hineingenangen.",
+               "Egal wie gut du fährst, Züge fahren Güter.",
+               "Meine Oma arbeitet beim FBI. Wir nennen Sie jetzt Top-Siegrid.",
+               "Warum können Geister so schlecht lügen? Weil man durch sie hindurchsehen kann."],
+
+        "en": ["Why do programmers prefer dark mode? Because the light attracts bugs!",
+                "Why was the math book sad? It had too many problems.",
+                "Why don’t skeletons fight each other? They don’t have the guts.",
+                "Why did the coffee file a police report? It got mugged."]
+    }
+    joke = random.choice(jokes.get(language if language else "de", jokes["de"]))
+    return {"joke": joke}
